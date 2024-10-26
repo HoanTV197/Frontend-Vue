c4486ed529435dcb59e2532a01402b9bc3743c6c
HoanTV <dlpytago197@gmail.com>
2024-10-26 14:55:30 +0700

    setup Ant design

--- src/main.js
+++ src/main.js
@@ -5,10 +5,13 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import 'ant-design-vue/dist/antd.css' // Import Ant Design styles
+import Antd from 'ant-design-vue' // Import thư viện
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Antd) // Sử dụng Ant Design Vue
 
 app.mount('#app')
